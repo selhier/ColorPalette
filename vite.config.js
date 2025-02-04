880ecb0eabe63efd3028f1027f15b5fdfc9a8e2e
Selhier <harykoshinigami@gmail.com>
2025-02-03 23:30:37 -0400

    fix: update asset paths in index.html to point to the assets directory and configure Vite to use an assets directory

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ColorPalette/', // Reemplaza 'tu-repositorio' con el nombre de tu repositorio
   build: {
-    outDir: 'docs', // Configura la carpeta de salida a 'docs'
+    outDir: 'docs',
+    assetsDir: 'assets'
   },
 });
