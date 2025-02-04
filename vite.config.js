0080288176e3beacae23c9280442bcde322e67e8
Selhier <harykoshinigami@gmail.com>
2025-02-03 23:53:38 -0400

    fix: update asset paths in index.html and Vite config for correct referencing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ColorPalette/', // Reemplaza 'tu-repositorio' con el nombre de tu repositorio
+  base: 'ColorPalette/', // Reemplaza 'tu-repositorio' con el nombre de tu repositorio
   build: {
     outDir: 'docs', // Configura la carpeta de salida a 'docs'
   },
